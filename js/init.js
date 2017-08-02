3706510f79722cdfb33a95756085ed879e43f051
abdullah-nsr <abdullah.nasri@outlook.sa>
2017-08-01 13:00:45 -1200

    fix for screen (max-width: 767px)

--- js/init.js
+++ js/init.js
@@ -10,9 +10,7 @@ jQuery(document).ready(function($) {
     /* FitText Settings
     ------------------------------------------------------ */
 
-    setTimeout(function() {
-        $('h1.responsive-headline').fitText(1, { minFontSize: '40px', maxFontSize: '90px' });
-    }, 100);
+
 
 
     /*----------------------------------------------------*/
